6a92d38c8cd36133b5bd91debf3f43f8ee987de8
Clouds Flowing <clouds.flowing@gmail.com>
2024-09-20 16:28:51 +0800

    update unpack script

--- scripts/_common.py
+++ scripts/_common.py
@@ -1,6 +1,29 @@
 # %%
 import os
+import logging
 
 def enter_project_root():
   while not os.path.exists('pixi.toml'):
     os.chdir('..')
+
+def get_logger(name: str, *, level: int = logging.INFO, filename: str | None = None) -> logging.Logger:
+  logger = logging.getLogger(name)
+  logger.setLevel(level)
+
+  if filename is not None:
+    import pathlib
+    pathlib.Path(filename).parent.mkdir(parents=True, exist_ok=True)
+    ch = logging.FileHandler(filename, mode='a')
+  else:
+    ch = logging.StreamHandler()
+  ch.setLevel(level)
+  formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+  ch.setFormatter(formatter)
+  logger.addHandler(ch)
+  return logger
+
+def today_str() -> str:
+  import datetime
+  return str(datetime.date.today()).replace('-', '')
+
+# %%
--- scripts/unpack.py
+++ scripts/unpack.py
@@ -1,22 +1,25 @@
 # %%
+from _common import *
+
 from dataclasses import dataclass
-from _common import*
+import polars as pl
 import pythonnet
 import sys, pathlib
 enter_project_root()
+logger = get_logger(__name__, filename=f'logs/unpack_{today_str()}.log')
 pythonnet.load("coreclr")
 sys.path.append(str(pathlib.Path("libs/UnpackHelper/bin/Debug/net8.0").absolute()))
 import clr
-import importlib
 for dll in ["UnpackHelper", "SoulsFormats"]:
   clr.AddReference(dll)
-  module = importlib.import_module(dll)
-  importlib.reload(module)
 import SoulsFormats # type: ignore
 import UnpackHelper # type: ignore
-key = UnpackHelper.Helper.GetKey(SoulsFormats.BHD5.Game.EldenRing, "DLC")
-files = UnpackHelper.Helper.GetDictionary(SoulsFormats.BHD5.Game.EldenRing)
-files.Count
+import System.IO # type: ignore
+
+src_path = "/mnt/d/Steam/steamapps/common/ELDEN RING/Game"
+dst_dir = "/mnt/d/tmp/ER"
+tags = ["Data0", "Data1", "Data2", "Data3", "DLC"]
+game = SoulsFormats.BHD5.Game.EldenRing
 
 # %%
 @dataclass
@@ -60,38 +63,79 @@ class Field:
   def to_dict(fields: list["Field"], instance):
     return {f.name: getattr(instance, f.name) for f in fields}
 
+@dataclass
+class FileHeader:
+  FileNameHash: int
+  PaddedFileSize: int
+  UnpaddedFileSize: int
+  FileOffset: int
+  SHAHash: str | None
+  AESKey: str | None
+
+  @staticmethod
+  def from_net(f: SoulsFormats.BHD5.FileHeader):
+    return FileHeader(
+      FileNameHash=f.FileNameHash,
+      PaddedFileSize=f.PaddedFileSize,
+      UnpaddedFileSize=f.UnpaddedFileSize,
+      FileOffset=f.FileOffset,
+      SHAHash=System.Convert.ToHexString(f.SHAHash.Hash) if f.SHAHash else None,
+      AESKey=System.Convert.ToHexString(f.AESKey.Key) if f.AESKey else None,
+    )
+
+def unpack_bhd(path: str, tag: str, hashes: pl.DataFrame):
+  key = UnpackHelper.Helper.GetKey(game, tag)
+  bhd5 = UnpackHelper.Helper.OpenHeader(game, path, key)
+  file_headers = [f for b in bhd5.Buckets for f in b]
+  df = pl.DataFrame([FileHeader.from_net(i) for i in file_headers], schema_overrides={'FileNameHash': pl.UInt64})
+  df = df.join(hashes, left_on='FileNameHash', right_on='hash', how='left')
+  return df, file_headers
+
+def unpack_bdt(path: str, dst_dir: str, *, file_headers: list[SoulsFormats.BHD5.FileHeader], df: pl.DataFrame, progress: bool = True):
+  stream = System.IO.File.OpenRead(path.replace(".bhd", ".bdt"))
+  total_bytes = df['PaddedFileSize'].sum()
+  tag = path.split('/')[-1].split('\\')[-1]
+  if progress:
+    import tqdm
+    bar = tqdm.tqdm(total=total_bytes, unit='B', unit_scale=True, desc=tag)
+  for (f, r) in zip(file_headers, df.rows(named=True)):
+    if r['path'] is None:
+      logger.warning(f"[{tag}] Unknown name {r['FileNameHash']}")
+      continue
+    target_path = pathlib.Path(f"{dst_dir}/{r['path']}")
+    if target_path.exists():
+      logger.info(f"[{tag}] Skipping {target_path}")
+      if progress:
+        bar.update(r['PaddedFileSize'])
+      continue
+    try:
+      data = f.ReadFile(stream)
+      size = len(data) # data.Length
+    except Exception as e:
+      logger.error(f"[{tag}] Failed to read {r['path']}")
+      raise e
+    pathlib.Path(target_path).parent.mkdir(parents=True, exist_ok=True)
+    try:
+      System.IO.File.WriteAllBytes(str(target_path), data)
+    except Exception as e:
+      logger.error(f"[{tag}] Failed to write {target_path}")
+      os.remove(target_path)
+      raise e
+    logger.info(f"[{tag}] {target_path} size={size} hash={r['SHAHash']}")
+    if progress:
+      bar.update(r['PaddedFileSize'])
+
 # %%
-import polars as pl
+files = UnpackHelper.Helper.GetDictionary(game)
 df_hash = pl.DataFrame([{'path':v,'hash':k} for k, v in dict(files).items()], schema_overrides={'hash': pl.UInt64})
 df_hash
 
 # %%
-path = "/mnt/d/Steam/steamapps/common/ELDEN RING/Game/DLC.bhd"
-bhd5 = UnpackHelper.Helper.OpenHeader(SoulsFormats.BHD5.Game.EldenRing, path, key)
-
-# %%
-bhd5.Buckets.Count
-
-# %%
-file_headers = [f for b in bhd5.Buckets for f in b]
-fields = Field.collect_type(file_headers[0].GetType())
-df = pl.DataFrame([Field.to_dict(fields, i) for i in file_headers], schema_overrides={f.name: pl.String if f.type == "String" else pl.UInt64 for f in fields})
-df = df.join(df_hash, left_on='FileNameHash', right_on='hash', how='left')
-df
-
-# %%
-import System.IO # type: ignore
-stream = System.IO.File.OpenRead(path.replace(".bhd", ".bdt"))
-dst_dir = pathlib.Path("/mnt/d/tmp/ER")
-logger = open("log.txt", "w")
-for (f, r) in zip(file_headers, df.rows(named=True)):
-  if r['path'] is None:
-    continue
-  target_path = dst_dir.joinpath(str(r['path']).removeprefix('/'))
-  print(target_path, file=logger)
-  data = f.ReadFile(stream)
-  target_path.parent.mkdir(parents=True, exist_ok=True)
-  System.IO.File.WriteAllBytes(str(target_path), data)
-logger.close()
+for tag in tags:
+  path = f"{src_path}/{tag}.bhd"
+  logger.info(f"Unpacking {path}")
+  df, file_headers = unpack_bhd(path, tag, hashes=df_hash)
+  print(df)
+  unpack_bdt(path, dst_dir, file_headers=file_headers, df=df)
 
 # %%
